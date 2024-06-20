b84a0764eb233cf657f9cb1323faa1593e68f9e2
Mihailo Ilic <mihailo.ilic@vizlore.com>
2024-06-20 13:38:24 +0200

    Added connection status check

--- examples/00_setup_DAClient.py
+++ examples/00_setup_DAClient.py
@@ -6,7 +6,11 @@
 """
 from sd_data_adapter.client import DAClient
 
+from sd_data_adapter.api import is_alive_check
+
 if __name__ == '__main__':
     HOST: str = "localhost"
     PORT: int = 1026
     DAClient.get_instance(HOST, PORT)
+
+    print("Connection is alive:", is_alive_check())
--- sd_data_adapter/api/__init__.py
+++ sd_data_adapter/api/__init__.py
@@ -2,3 +2,4 @@ from .upload import upload
 from .update import update, upsert
 from .search import search
 from .search import get_by_id
+from .misc import is_alive_check
