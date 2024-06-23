aba3f81e3fc7373660f1262db25ceb5137a32640
Petar Maletin <68387112+ppettarrm@users.noreply.github.com>
2024-06-23 13:22:59 +0200

    Removed redundant if checks in transformational methods, added tests (#13)
    * Removed redundant if checks in transformational methods, added tests
    
    * Added tests, fixed typo in AgriCrop

---
+++ test/test_models/ModelTests.py
@@ -0,0 +1,28 @@
+import unittest
+
+from sd_data_adapter.models.agri_food import AgriFarm
+
+class ModelTests(unittest.TestCase):
+
+    def test_minimal_object_setup(self):
+        '''
+        An instance of any SmartDataModel needs to have
+        an ID and Type.
+        '''
+
+        a = AgriFarm()
+
+        self.assertEqual(AgriFarm.__name__, a.type)
+        self.assertIsNotNone(a.id)
+
+    def test_id_not_overwritten(self):
+
+        id_val = "new_id"
+
+        a = AgriFarm(id=id_val)
+
+        self.assertEqual(a.id, id_val)
+
+
+if __name__ == '__main__':
+    unittest.main()
