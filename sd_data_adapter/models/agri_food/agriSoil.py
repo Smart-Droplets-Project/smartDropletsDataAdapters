4b55c351373d96284df97a99a03a2aee44b568e0
Michiel Kallenberg <40028714+michielkallenberg@users.noreply.github.com>
2024-09-16 09:14:28 +0200

    pest -> soil (#19)

--- sd_data_adapter/models/agri_food/agriSoil.py
+++ sd_data_adapter/models/agri_food/agriSoil.py
@@ -10,22 +10,22 @@ class AgriSoil(AgriFood):
     Class representing an agricultural soil.
 
     Attributes:
-        id (str): The unique identifier of the pest.
-        type (str): The type of the pest.
-        name (str): The name of the pest.
+        id (str): The unique identifier of the soil.
+        type (str): The type of the soil.
+        name (str): The name of the soil.
 
     Optional Attributes:
-        agroVocConcept (Optional[str]), default=None): Reference to the agrovoc term associated with this pest.
-        alternateName (Optional[str], default=None): Alternate name of the pest.
-        dataProvider (Optional[str], default=None): The data provider of the pest.
-        dateCreated (Optional[str], default=None): The date when the pest data was created.
-        dateModified (Optional[str], default=None): The date when the pest data was last modified.
-        description (Optional[str], default=None): Description of the pest.
-        hasAgriProductType (Optional[List[AgriProductType] | str], default=None): List of agri product types associated with the pest.
-        owner (Optional[str], default=None): Owner of the pest.
-        relatedSource (Optional[List[str]], default=None): List of related sources of the pest.
-        seeAlso (Optional[List[str]], default=None): List of see also references related to the pest.
-        source (Optional[List[str]], default=None): List of sources of the pest.
+        agroVocConcept (Optional[str]), default=None): Reference to the agrovoc term associated with this soil.
+        alternateName (Optional[str], default=None): Alternate name of the soil.
+        dataProvider (Optional[str], default=None): The data provider of the soil.
+        dateCreated (Optional[str], default=None): The date when the soil data was created.
+        dateModified (Optional[str], default=None): The date when the soil data was last modified.
+        description (Optional[str], default=None): Description of the soil.
+        hasAgriProductType (Optional[List[AgriProductType] | str], default=None): List of agri product types associated with the soil.
+        owner (Optional[str], default=None): Owner of the soil.
+        relatedSource (Optional[List[str]], default=None): List of related sources of the soil.
+        seeAlso (Optional[List[str]], default=None): List of see also references related to the soil.
+        source (Optional[List[str]], default=None): List of sources of the soil.
 
     """
 
